9a4c9d79fddce310ec8bd2af1516f7803109922b
BuyanauskasAA <Buyanauskas@gmail.com>
2023-09-13 17:47:07 +0600

    feat: ограничить частоту запросов с помощью express-rate-limit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { errors } = require('celebrate');
 
 const routes = require('./routes');
 const errorHandler = require('./middlewares/error-handler');
+const rateLimiter = require('./utils/rate-limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
@@ -22,6 +23,7 @@ mongoose
 const app = express();
 
 app.use(helmet());
+app.use(rateLimiter);
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(requestLogger);
