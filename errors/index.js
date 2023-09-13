9ef32e94b1eed7474be47709ec505307e1a692ba
BuyanauskasAA <Buyanauskas@gmail.com>
2023-09-13 16:47:29 +0600

    fix: исправить название класса ConflictError

--- errors/index.js
+++ errors/index.js
@@ -1,12 +1,12 @@
 const BadRequestError = require('./bad-request-err');
-const ConfilctError = require('./conflict-err');
+const ConflictError = require('./conflict-err');
 const ForbiddenError = require('./forbidden-err');
 const NotFoundError = require('./not-found-err');
 const UnauthorizedError = require('./unauthorized-err');
 
 module.exports = {
   BadRequestError,
-  ConfilctError,
+  ConflictError,
   ForbiddenError,
   NotFoundError,
   UnauthorizedError,
