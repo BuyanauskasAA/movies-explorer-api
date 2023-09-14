4c1f67531470b4d77e821d6340308b72030dccc7
BuyanauskasAA <Buyanauskas@gmail.com>
2023-09-14 20:51:34 +0600

    fix: добавить проверку авторизации в роуты

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
+const auth = require('../middlewares/auth');
 const authRouter = require('./auth');
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
 const { NotFoundError } = require('../errors');
 
 router.use('/', authRouter);
+router.use(auth);
 router.use('/users', usersRouter);
 router.use('/movies', moviesRouter);
 router.use('*', (req, res, next) => next(new NotFoundError('Неверный ULR-запрос к серверу!')));
