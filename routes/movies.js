63b2342ea99d52e22456d4547931181b17549464
BuyanauskasAA <Buyanauskas@gmail.com>
2023-09-15 11:14:10 +0600

    fix: валидировать movieId как число

--- routes/movies.js
+++ routes/movies.js
@@ -18,7 +18,7 @@ router.post(
       trailerLink: Joi.string().regex(urlRegex).required(),
       thumbnail: Joi.string().regex(urlRegex).required(),
       owner: Joi.string().hex().length(24).required(),
-      movieId: Joi.string().hex().length(24).required(),
+      movieId: Joi.number().required(),
       nameRU: Joi.string().required(),
       nameEN: Joi.string().required(),
     }),
